4f86173c3e7822dc323db02dc1e1b904b5058d64
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-09-23 18:25:20 -0400

    added utils

--- lib/logger.hpp
+++ lib/logger.hpp
@@ -1,1 +1,16 @@
+#pragma once
+#include <iomanip>
+#include <iostream>
+#include "./utils.hpp"
+#include "../src/types/syntax_t.hpp"
+
+inline void logToken(const Token& tok) {
+  std::cout 
+    << std::left << std::setw(20) << "Token"
+    << "| type: " << std::setw(25) << tokenTypeToString(tok.tok_type)
+    << "| val: "  << std::setw(15) << ("\"" + tok.tok_val + "\"")
+    << "| row: "  << std::setw(4)  << tok.tok_row
+    << "| col: "  << std::setw(4)  << tok.tok_col
+    << std::endl;
+}
 
