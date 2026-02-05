266550bbd721fb554ca03129aef71fee22a0403d
Daniel S Jeremiah <danielshunom2@gmail.com>
2026-02-05 01:06:47 -0500

    redifned node

---
+++ src/nodes/node_expr.hpp
@@ -0,0 +1,57 @@
+#include "../tokens/token_types.hpp"
+#include "../symbols/symbol_types.hpp"
+#include "../typing/types.hpp"
+#include <memory>
+#include <vector>
+
+struct Node {virtual ~Node() = default;};
+
+struct FuncParam {
+  std::string param_name;
+  Type param_type;
+};
+
+using NDPtr = std::unique_ptr<Node>;
+
+struct NDLiteral: Node {
+  Token literal;
+};
+
+struct NDIdentifier: Node {
+  Symbol* identifier_symbol;
+  Token identifier;
+};
+
+struct NDLetBindExpr: Node {
+  std::unique_ptr<NDIdentifier> identifier;
+  NDPtr bound_value;
+};
+
+struct NDConstExpr: Node {
+  std::unique_ptr<NDIdentifier> identifier;
+  NDLiteral literal;
+};
+
+struct NDCallExpr: Node {
+  std::unique_ptr<NDIdentifier> identifier;
+  std::vector<NDPtr> args;
+};
+
+struct NDFuncDeclExpr: Node {
+  Token func_identifier;
+  std::vector<FuncParam> func_params;
+  std::vector<NDPtr> func_body;
+};
+
+struct NDCaseExpr: Node {
+  struct Branch {
+    std::vector<NDPtr> pattern;
+    NDPtr result;
+  };
+  std::vector<NDPtr> conditions;
+  std::vector<Branch> branches;
+};
+
+struct Parent {
+  std::vector<NDPtr> children;
+};
