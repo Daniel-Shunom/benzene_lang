61ed2ab55d908c8e547bed97da99cf635cf8fb3c
Daniel S Jeremiah <danielshunom2@gmail.com>
2026-02-09 18:01:24 -0500

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,25 @@
 #include "../lib/files.hpp"
+#include "ast/print/print.hpp"
+#include "ast/sym_res/sym_res.hpp"
 #include "cmd/cmd.hpp"
 #include "lexer/lexer.hpp"
 #include "parsers/combinator/parser_types.hpp"
 #include "parsers/combinator/parsers.hpp"
-#include "tokens/token_types.hpp"
 #include <cstdio>
-#include <print>
 #include <string_view>
 
 int main(int argc, char* argv[]) {
+
   Args args = GetArgs(argc, argv);
   std::string src_code = FileToString(args.file_path);
   std::string_view contents{src_code};
   Lexer lexer(contents);
- 
-  std::printf(
-    "\nLexing current file\n"
-    "File path: %s\n",
-    args.file_path.data()
-  );
-  
-  if (args.is_logs_enabled()) {
-    std::printf("[LOGS ENABLED]\n");
-  }
+
+  std::printf( "\nLexing current file\n" "File path: %s\n", args.file_path.data());
+
+  if (args.is_logs_enabled()) std::printf("[LOGS ENABLED]\n");
 
   lexer.scan_tokens();
-  lexer.print_tokens();
 
   auto tokens = lexer.get_tokens();
   auto state = ParserState();
@@ -35,7 +29,7 @@ int main(int argc, char* argv[]) {
   std::printf("Beginning AST Parsing\n");
 
   PResult<Parent> parent = run_parser(state);
-  
+
   if(!parent) {
     std::printf("Parsing failed, Displaying Parse Logs\n");
     state.display_logs();
@@ -48,6 +42,13 @@ int main(int argc, char* argv[]) {
     );
     state.display_logs();
     state.display_captured_exprs();
+
+    TreePrinter printer;
+    SymResolver resolver;
+
+    parent->add_visitor(resolver);
+    parent->add_visitor(printer);
+    parent->apply_visitors();
   }
 
   return 0;
