5a8ff94b050fa192fb3606a9eb5ddf576c1f2768
Daniel S Jeremiah <danielshunom2@gmail.com>
2026-02-11 15:41:42 -0500

    boiler plate for type checking module

--- src/ast/type_check/type_check.hpp
+++ src/ast/type_check/type_check.hpp
@@ -1,1 +1,19 @@
+#pragma once
+#include "../../nodes/node_visitor.hpp"
+#include "../../nodes/node_expr.hpp"
 
+class TypeChecker: public Visitor {
+public:
+  void visit(NDLiteral&) override;
+  void visit(NDImportDirective&) override;
+  void visit(NDIdentifier&) override;
+  void visit(NDLetBindExpr&) override;
+  void visit(NDConstExpr&) override;
+  void visit(NDCallExpr&) override;
+  void visit(NDCallChain&) override;
+  void visit(NDFuncDeclExpr&) override;
+  void visit(NDCaseExpr&) override;
+  void visit(NDBinaryExpr&) override;
+  void visit(NDUnaryExpr&) override;
+  void visit(NDScopeExpr&) override;
+};
