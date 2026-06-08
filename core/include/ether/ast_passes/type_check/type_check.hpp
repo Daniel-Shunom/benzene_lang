d8e5ee1f421d37fcbf8b005cbd07cd4c4ad285d0
shunom <danielshunom2@gmail.com>
2026-06-08 03:50:16 -0400

    update: added var factory member

--- core/include/ether/ast_passes/type_check/type_check.hpp
+++ core/include/ether/ast_passes/type_check/type_check.hpp
@@ -1,9 +1,16 @@
 #pragma once
+#include "ether/ast_passes/type_check/modules/module.hpp"
 #include <ether/nodes/node_visitor.hpp>
 #include <ether/nodes/node_expr.hpp>
+#include <ether/types/types.hpp>
+#include <memory>
+#include <vector>
 
 class TypeChecker: public Visitor {
 public:
+  TypeChecker();
+  void dispatch_to_modules(Node&);
+
   void visit(NDLiteral&)          override;
   void visit(NDImportDirective&)  override;
   void visit(NDIdentifier&)       override;
@@ -19,4 +26,9 @@ public:
   void visit(NDTupleExpr&)        override;
   void visit(NDListExpr&)         override;
   void visit(NDLambdaExpr&)       override;
+
+private:
+  TypeVarFactory varFactory;
+  std::vector<std::unique_ptr<TCModule>> modules;
 };
+
