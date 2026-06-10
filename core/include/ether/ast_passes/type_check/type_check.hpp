43b31c00678b941e2d65b82e5a05f7456d485d42
Daniel S Jeremiah <danielshunom2@gmail.com>
2026-06-10 00:33:05 -0400

    update: named params

--- core/include/ether/ast_passes/type_check/type_check.hpp
+++ core/include/ether/ast_passes/type_check/type_check.hpp
@@ -11,24 +11,25 @@ public:
   TypeChecker();
   void dispatch_to_modules(Node&);
 
-  void visit(NDLiteral&)          override;
-  void visit(NDImportDirective&)  override;
-  void visit(NDIdentifier&)       override;
-  void visit(NDLetBindExpr&)      override;
-  void visit(NDConstExpr&)        override;
-  void visit(NDCallExpr&)         override;
-  void visit(NDCallChain&)        override;
-  void visit(NDFuncDeclExpr&)     override;
-  void visit(NDCaseExpr&)         override;
-  void visit(NDBinaryExpr&)       override;
-  void visit(NDUnaryExpr&)        override;
-  void visit(NDScopeExpr&)        override;
-  void visit(NDTupleExpr&)        override;
-  void visit(NDListExpr&)         override;
-  void visit(NDLambdaExpr&)       override;
+  void visit(NDLiteral& expr)          override;
+  void visit(NDImportDirective& expr)  override;
+  void visit(NDIdentifier& expr)       override;
+  void visit(NDLetBindExpr& expr)      override;
+  void visit(NDConstExpr& expr)        override;
+  void visit(NDCallExpr& expr)         override;
+  void visit(NDCallChain& expr)        override;
+  void visit(NDFuncDeclExpr& expr)     override;
+  void visit(NDCaseExpr& expr)         override;
+  void visit(NDBinaryExpr& expr)       override;
+  void visit(NDUnaryExpr& expr)        override;
+  void visit(NDScopeExpr& expr)        override;
+  void visit(NDTupleExpr& expr)        override;
+  void visit(NDListExpr& expr)         override;
+  void visit(NDLambdaExpr& expr)       override;
+  void visit(NDFuncParam& expr)        override;
 
-private:
   TypeVarFactory varFactory;
+private:
   std::vector<std::unique_ptr<TCModule>> modules;
 };
 
